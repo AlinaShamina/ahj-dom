81134bc783b483156fc4f37a2c4a2a63010f2cc9
Neto test <alukovnikova777@gmail.com>
2025-11-24 18:29:44 +0300

    Fix package.json and webpack.config.js to auto-open browser and run dev server

--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,26 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true, // очищает dist при новой сборке
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: { loader: "babel-loader" },
       },
       {
         test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-        ],
+        use: [{ loader: "html-loader" }],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
+        use: [{ loader: "file-loader" }],
       },
       {
         test: /\.css$/,
@@ -44,11 +37,20 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "./index.html",
+      filename: "index.html",
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
-      chunkFilename: "[id].css",
     }),
   ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    open: true,  // автоматически открывает браузер
+    hot: true,   // горячая перезагрузка
+  },
+  mode: "development",
 };
